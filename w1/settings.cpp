134949837c96b4889bfb21033d95e7ce4cca2a16
jabhandari <jabhandari@myseneca.ca>
2024-09-25 09:53:54 -0400

    w1

--- w1/settings.cpp
+++ w1/settings.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "settings.h"
 namespace seneca {
 	Settings g_settings{};
--- w1/timeMonitor.cpp
+++ w1/timeMonitor.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "timeMonitor.h"
 #include<chrono>
 namespace seneca {
